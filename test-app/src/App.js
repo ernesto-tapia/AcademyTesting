aec9c61624bfae8fee24a8748000f56d5afa9b63
Ernesto Tapia <etapia@NSL-ETAPIAs-MacBook-Pro.local>
2020-06-08 22:18:34 -0700

    Updated the React Test

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
-
 function App() {
+  let color=[0,0,0];
+  let RGB;
+  setInterval(()=>{
+    color=[Math.floor(Math.random()*255)+1, Math.floor(Math.random()*255)+1, Math.floor(Math.random()*255)+1]
+    RGB=color[0]+','+color[1]+','+color[2];
+    const element= 
+    <div style={{backgroundColor:'rgba('+RGB+')'}}>
+      <p >this was the color in RGB:{RGB}</p>
+    </div> ;
+    ReactDOM.render(element, document.getElementById('color'));
+  },2000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is just an example of how react works, verify with F12 to see the console logs
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <p>the next will be modified by ReactDOM.render(),  it will change every 2 seconds</p>
+          <div className='center'>
+            <div id='color' className='color'/>
+          </div>
+        </div>
     </div>
   );
 }
